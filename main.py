9f037f98189bf908b037485970649c61e0fca10b
GMG666 <telematic@gmx.at>
2025-08-02 14:43:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import machine, sys
+import Temp
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/<GMG666>/<Updater_ESP32>/<main>"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
+
+
+try:
+    Temp.main()
+except Exception as e:
+    print("Schwerer Fehler in Main:")
+    sys.print_exception(e)
+
+# Following a normal Exception or main() exiting, reset the board.
+# Following a non-Exception error such as KeyboardInterrupt (Ctrl-C),
+# this code will drop to a REPL. Place machine.reset() in a finally
+# block to always reset, instead.
+machine.reset()
