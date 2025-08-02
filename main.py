6462b0a81fcb3087369ec525db67271b6f2ba7e6
GMG666 <telematic@gmx.at>
2025-08-02 15:04:14 +0200

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Temp
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://raw.githubusercontent.com/<GMG666>/<Updater_ESP32>/<main>"
+firmware_url = "https://raw.githubusercontent.com/GMG666/Updater_ESP32/main/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
